610ba98d66edb94e0f71380562c2cb3ac4a816a1
anesdal <66341254+anesdal@users.noreply.github.com>
2026-03-07 07:38:40 +0100

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,10 +1,4 @@
 pluginManagement {
-    val flutterSdkPath = System.getenv("FLUTTER_ROOT")
-    
-    if (flutterSdkPath != null) {
-        includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-    }
-
     repositories {
         google()
         mavenCentral()
@@ -13,7 +7,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
