2fdd7e6dc13912eef708b3a35411453047b20017
anesdal <66341254+anesdal@users.noreply.github.com>
2026-03-07 04:38:28 +0100

    Refactor flutterSdkPath loading in settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,10 +1,8 @@
 pluginManagement {
     def flutterSdkPath = {
-        def properties = new Properties()
-        file("local.properties").withInputStream { properties.load(it) }
-        def flutterSdkPath = properties.getProperty("flutter.sdk")
-        assert flutterSdkPath != null, "flutter.sdk not set in local.properties"
-        return flutterSdkPath
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        properties.getProperty("flutter.sdk") ?: throw GradleException("flutter.sdk not set in local.properties")
     }()
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
@@ -17,9 +15,9 @@ pluginManagement {
 }
 
 plugins {
-    id "dev.flutter.flutter-gradle-plugin" version "1.0.0" apply false
-    id "com.android.application" version "8.1.0" apply false
-    id "org.jetbrains.kotlin.android" version "2.1.0" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
-include ":app"
+include(":app")
